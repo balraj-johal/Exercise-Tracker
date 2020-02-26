e26d21dda973db59bdbc5ddb704342c3dddc52bd
balraj-johal <balraj-johal@users.noreply.github.com>
2020-02-26 19:57:50 +0000

    connect to mongodb atlas

--- mern-excercise-tracker/server/server.js
+++ mern-excercise-tracker/server/server.js
@@ -11,11 +11,11 @@ app.use(cors())
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 
 connection.once('open', () => {
-  console.log('MONGODB ACCESED ! ..  ')
+  console.log('MONGODB ACCESED ! .. 4 ')
 })
 
 app.listen(port, () => {
